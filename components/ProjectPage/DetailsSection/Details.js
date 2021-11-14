9280d30272c913e12972fed969aff7fa427d98b7
Alvaro <995.alvaro@gmail.com>
2021-11-15 00:03:31 +0100

    mobile menu re-styled

--- components/ProjectPage/DetailsSection/Details.js
+++ components/ProjectPage/DetailsSection/Details.js
@@ -16,26 +16,31 @@ const Details = () => {
         <div className={classes.info}>
           <div>
             <div className={classes.text_section}>
-              <h3 className={classes.info__title}>Project X</h3>
+              <h3 className={classes.info__title}>Car-Auctions</h3>
               <p className={classes.info__description}>
                 Web application that allows users to buy and sell cars by using
                 an auctions system together with a bunch of other features.
               </p>
               <p className={classes.info__description}>
                 These are the functionalities/features of this web application:
-                <ul>
-                  <li>Full Responsive</li>
-                  <li>Login/Logout</li>
-                  <li>Auto-Logout after 1h session</li>
-                  <li>Create car auctions</li>
-                  <li>Place bids on existing auctions</li>
-                  <li>Add auction to favourites</li>
-                  <li>The remaining time for each auction is displayed in real time</li>
-                  <li>When an auction ends, the name of the user that placed the last bid is displayed as a winner</li>
-                  <li>Filtering of results by auction state and price range</li>
-                  <li>Contact us form</li>
-                </ul>
               </p>
+              <ul className={classes.info__description}>
+                <li>Full Responsive</li>
+                <li>Login/Logout</li>
+                <li>Auto-Logout after 1h session</li>
+                <li>Create car auctions</li>
+                <li>Place bids on existing auctions</li>
+                <li>Add auction to favourites</li>
+                <li>
+                  The remaining time for each auction is displayed in real time
+                </li>
+                <li>
+                  When an auction ends, the name of the user that placed the
+                  last bid is displayed as a winner
+                </li>
+                <li>Filtering of results by auction state and price range</li>
+                <li>Contact us form</li>
+              </ul>
             </div>
             <div className={classes.tool_section}>
               <h3 className={classes.info__title}>Tools Used</h3>
