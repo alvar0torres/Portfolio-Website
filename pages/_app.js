833564c363acb0e2e7e6ca31876cd1521a6b6f37
Alvaro <995.alvaro@gmail.com>
2021-11-16 21:18:57 +0100

    added Google Analytics tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Script from "next/script";
+
+import { Fragment } from "react";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Script
+        strategy="lazyOnload"
+        src={"https://www.googletagmanager.com/gtag/js?id=G-V66VBH7T18"}
+      />
+      <Script id="gtag-init" strategy="lazyOnload">
+        {`
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+     gtag('config', 'G-V66VBH7T18');`}
+      </Script>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
